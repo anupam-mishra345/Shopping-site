5069fbf4ed037524ac0270c67ca9c6ee5f4e6ede
Anupam Mishra <anupam.mishra345@gmail.com>
2021-03-12 13:46:53 +0530

    Update landing.js
    some functionality added

--- landing.js
+++ landing.js
@@ -1,11 +1,13 @@
 var index;
+var data;
 fetch('items.json')
 .then(function(response){
     return response.json();
 })
-.then( (data)=>{
+.then( (mydata)=>{
     let param=new URL(document.location).searchParams;
     index=param.get("id")-1;
+    data=mydata;
     appendData(data);
 })
 .catch(function (err){
@@ -17,13 +19,18 @@ function appendData(data){
 
     var detail=data[index].images;
     
-    document.getElementById("img1").src=detail[0];
+    let imgVar=document.getElementById("img1")
+    imgVar.src=detail[0];
+    // imgVar.style.width="auto";
+    // imgVar.style.height="400px";
+    
 
     var par=document.getElementById("img2");    
         for(var i=0;i<detail.length;i++){
             var ch=document.createElement("img");
             ch.src=detail[i];
-            ch.classList.add("imgAttr")
+            ch.classList.add("imgAttr");
+            ch.setAttribute("onclick","myImage('"+i+"')");//"`myImage(${data},${index},${i})`");
             par.appendChild(ch);       
         }
 
@@ -32,12 +39,12 @@ function appendData(data){
         name[i].innerHTML+=data[index].name;
 
     }
-    document.getElementById("price").innerHTML="Rs."+data[index].price + "  "+`<span style="font-size:20px;color:black">(inclusive of all taxes)</span>`;
+    document.getElementById("price").innerHTML="&#8377;"+data[index].price + "  "+`<span style="font-size:20px;color:black">(inclusive of all taxes)</span>`;
     var rating=document.getElementsByClassName("rating")
     for(var i=0;i<rating.length;i++){
-        rating[i].innerHTML+="Ratings: "+data[index].rating+`<img src="rating-star-icon-11-256.png" style="margin-bottom:10px" height="30px" width="30px">`;
+        rating[i].innerHTML+="Ratings: "+data[index].rating+`<img src="rating-star-icon-11-256.png" style="margin-bottom:7px" height="24px" width="24px">`;
     }
-    document.getElementById("rating").innerHTML+=data[index].rating+`<img src="rating-star-icon-11-256.png" style="margin-bottom:10px" height="30px" width="30px">`   
+    document.getElementById("rating").innerHTML+=data[index].rating+`<img src="rating-star-icon-11-256.png" style="margin-bottom:7px" height="24px" width="24px">`   
     document.getElementById("brand").innerHTML+=data[index].brand;
     document.getElementById("category").innerHTML+=data[index].type;
     document.getElementById("gender").innerHTML+=data[index].department;
@@ -58,29 +65,60 @@ function appendData(data){
     }
 }
 
-function setImages(data,index,i){
+function myImage(i) {
+    document.getElementById("img1").src="";
     document.getElementById("img1").src=data[index].images[i];
+    document.getElementById("img1").focus();
+
 }
 
 function myCart(){
+let s=document.getElementById("sz").value
 
+if (s==="") {
+    alert("Please enter size");
+}
+else{
     if (localStorage.getItem('carts') == null) {
         
         let cart=[];
-        let size=cart.length;
-        cart[size]=index;
+        let size=[];
+        let qnty=[];
+        
+        cart[cart.length]=index;
+        size[size.length]=s;
+        qnty[qnty.length]=1;
         localStorage.setItem("carts",JSON.stringify(cart));
+        localStorage.setItem("sizes",JSON.stringify(size));
+        localStorage.setItem("quantity",JSON.stringify(qnty));
         console.log(cart);
 
     }
     else{
         let cart=localStorage.getItem("carts");
         //console.log(JSON.parse(cart));
-        let m=JSON.parse(cart)
-        let p=m.length
-        m[p]=index;
+        let size=localStorage.getItem("sizes");
+        let m=JSON.parse(cart);
+        let sz=JSON.parse(size);
+        let qnty=JSON.parse(localStorage.getItem("quantity"));
+        let p=m.length;
+        let temp=0;
+        for(let i=0;i<p;i++){
+            if(m[i]==index){
+                temp++;
+            }
+        }
+        if (temp==0) {
+            m[p]=index;
+            sz[sz.length]=s;
+            qnty[qnty.length]=1;
+        }
         localStorage.setItem("carts",JSON.stringify(m));
-        
+        localStorage.setItem("sizes",JSON.stringify(sz));
+        localStorage.setItem("quantity",JSON.stringify(qnty));
     }
+    
     window.document.location="cart.html"+"?id="+(index+1);
+
+}
 }
